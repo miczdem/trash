60398fa8f4bbd9e199f99250a4e7d9a6d9b03c9c
Michael Demarco <miczdem@gmail.com>
2014-11-25 02:53:29 +1100

    modified app user

--- Mic.Trash.Domain/ApplicationUser.cs
+++ Mic.Trash.Domain/ApplicationUser.cs
@@ -13,10 +13,9 @@ namespace Mic.Trash.Domain
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public const string AdminRole = "Administrator";
-        [Required]
+        
         public string FirstName { get; set; }
-        [Required]
+        
         public string LastName { get; set; }
         public string FullName
         {
