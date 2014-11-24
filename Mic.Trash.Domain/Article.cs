9ce6643fa4b3820ec75653bc941b1ac82428a2c4
Michael Demarco <miczdem@gmail.com>
2014-11-24 22:12:13 +1100

    initial commit

---
+++ Mic.Trash.Domain/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mic.Trash.Domain
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Mic.Trash.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mic.Trash.WebUI.Startup))]
+namespace Mic.Trash.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
